e15f16b49e0ae571259158a42f19a83e5155488f
prief <207792089@qq.com>
2022-04-27 23:02:41 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <Sidebar></Sidebar>
+    <div id="nav">
+      <router-link to="/">
+        <svg-icon icon-class="denglong"></svg-icon>Home
+      </router-link>|
+      <router-link to="/about">About</router-link>
+    </div>
+
+    <!-- 路由出口 -->
+    <router-view />
+  </div>
+</template>
+
+<script>
+import Sidebar from "@/components/Sidebar";
+
+export default {
+  components: {
+    Sidebar
+  }
+};
+</script>
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
